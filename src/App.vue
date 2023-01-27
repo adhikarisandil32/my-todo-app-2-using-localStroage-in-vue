619482791ed9e6cff56527df66907b36fe3b1791
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-01-27 16:26:15 +0545

    m

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <form @submit.prevent="addNewTodo">
+      Add a todo:
+      <input type="text" placeholder="e.g. make a video" v-model="newTodo">
+      <button>Add</button>
+    </form>
+    <TodoComponent :todoLists="todos"/> <!-- todos passed as props -->
+  </main>
+</template>
+
+<script setup>
+/*eslint-disable*/
+
+import TodoComponent from './components/TodoComponent.vue'
+import {ref} from 'vue'
+
+//boilerplate todos. its is the list of todos with id and title
+//this is where after click event, the id and title of todo is pushed
+const todos = ref([])
+let newTodoId = ref(0)
+
+const newTodo = ref('') //newTodos, specially titles
+
+function addNewTodo(){
+  if(newTodo.value.trim() === ""){
+    return
+  }
+  todos.value.push({
+    id: newTodoId.value++,
+    title: newTodo.value
+  })
+  newTodo.value = '';
+}
+
+</script>
+
+<style scoped>
+  main{
+    width: 600px;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 12px 24px 0 24px;
+    background-color: rgb(222, 236, 249);
+  }
+</style>
---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <!-- eslint-disable --> 
+        <h3>List of Todos: </h3>
+        <ul>
+            <li v-for="(eachTodo, idx) in todoLists" :key="eachTodo.id">
+                {{ eachTodo.title }}
+                <!-- <button @click="removeClicked(todoLists, key)">Remove</button> -->
+                <button @click="editClicked(idx)">Edit</button>
+                <button @click="removeClicked(idx)">Remove</button>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script setup>
+    /* eslint-disable */
+    
+    //this is how we receive props directly into script
+    const props = defineProps({todoLists:Object}) //or defineProps(['todoLists'])
+
+    //function for button with event action removeClicked(key)
+    let removeClicked = (removeFrom) => {
+        props.todoLists.splice(removeFrom, 1)
+    }
+
+    let editClicked = (editFrom) => {
+        const newInputText = prompt("Edit");
+        if(newInputText.trim() === ""){
+            return
+        }
+        props.todoLists[editFrom].title = newInputText;
+    }
+    //function for button with event action removeClicked(todoLists, key)
+    /* let removeClicked = (todoLists, removeFrom) => {
+        todoLists.splice(removeFrom, 1)
+    } */
+</script>
+
+<style>
+
+</style>
